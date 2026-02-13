72fa95c45bb1292e3187a062d2fb64c4db81cf6a
“AdityaNeeraje” <aditya.neeraje@gmail.com>
2026-02-14 00:47:01 +0530

    done with q4

--- q4/q4.py
+++ q4/q4.py
@@ -35,8 +35,8 @@ def myXrayIntegration(f,t,theta_deg,delta_s,interpolation_scheme=1):
     return coords
 
 def rrmse(image1, image2):
-    image1 = (image1 - image1.min()) / (image1.max() - image1.min())
-    image2 = (image2 - image2.min()) / (image2.max() - image2.min())
+    # image1 = (image1 - image1.min()) / (image1.max() - image1.min())
+    # image2 = (image2 - image2.min()) / (image2.max() - image2.min())
     return np.sqrt(np.sum((image1 - image2) ** 2) / np.sum(image1 ** 2))
 
 def integrate(f, coords):
@@ -49,41 +49,50 @@ def myART(ordered_projections, lr, num_iterations=100):
     num_projections = ordered_projections.shape[0]
     for iteration in range(num_iterations):
         latest_rrmses = []
-        for i in range(num_projections):
+        for i in trange(num_projections):
             t, theta = ordered_projections[i]
             coords = myXrayIntegration(reconstructed_image, t, theta, delta_s=1)
             current_projection = integrate(reconstructed_image, coords)
             ordered_projection = integrate(image1, coords)
             error = ordered_projection - current_projection
             correction = lr * error / len(coords[0])  
-            coords = np.round(coords).astype(int)
-            reconstructed_image[coords[0], coords[1]] += correction
-            rrmses.append(rrmse(image1, reconstructed_image))
-            latest_rrmses.append(rrmses[-1])
-            print(i, rrmses[-1])
-            # if len(latest_rrmses) > 100 and max(latest_rrmses[-101:-1]) - min(latest_rrmses[-101:-1]) < 1e-5:
+            floored = np.floor(coords).astype(int)
+            frac = coords - floored
+            ceiled = np.ceil(coords).astype(int)
+            wy = frac[0, :]
+            wx = frac[1, :]            
+            reconstructed_image[ceiled[0], ceiled[1]] += correction * (1 - wy) * (1 - wx)
+            reconstructed_image[floored[0], ceiled[1]] += correction * wy * (1 - wx)
+            reconstructed_image[ceiled[0], floored[1]] += correction * (1 - wy) * wx
+            reconstructed_image[floored[0], floored[1]] += correction * wy * wx
+            np.clip(reconstructed_image, 0, 1, out=reconstructed_image)
+            # latest_rrmses.append(rrmses[-1])
+            # print(i, rrmses[-1])
+            # # if len(latest_rrmses) > 100 and max(latest_rrmses[-101:-1]) - min(latest_rrmses[-101:-1]) < 1e-5:
+            # #     print("Convergence reached at iteration", iteration, "projection", i)
+            # #     break
+            # if len(latest_rrmses) > 100 and max(latest_rrmses[-101:-1]) < latest_rrmses[-1]:
             #     print("Convergence reached at iteration", iteration, "projection", i)
             #     break
-            if len(latest_rrmses) > 100 and max(latest_rrmses[-101:-1]) < latest_rrmses[-1]:
-                print("Convergence reached at iteration", iteration, "projection", i)
-                break
+        rrmses.append(rrmse(image1, reconstructed_image))
+        print(f"Iteration {iteration+1}/{num_iterations}, RRMSE: {rrmses[-1]:.6f}")
         # lr /= 10
-    with open('reconstructed_image.txt', 'w') as f:
-        for row in reconstructed_image:
-            f.write(' '.join(map(str, row)) + '\n')
+    # with open('reconstructed_image.txt', 'w') as f:
+    #     for row in reconstructed_image:
+    #         f.write(' '.join(map(str, row)) + '\n')
     plt.imshow(reconstructed_image, cmap='gray')
     plt.axis('off')
-    plt.savefig(f'reconstructed_image_lambda_{lr}.png')
+    plt.savefig(f'reconstructed_image_lambda_{lr}.png', bbox_inches='tight', pad_inches=0)
     plt.close()
     return reconstructed_image
 
 
-t = np.arange(-250, 250, 1)
+t = np.arange(-255, 255, 1)
 thetha = np.arange(0, 180, 1)
 coords = np.array(np.meshgrid(t, thetha)).reshape(2, -1).T
 import random
 random.shuffle(coords)
-lr=0.1
+
 radon_transform = radon(image1, theta=thetha, circle=False)
 iradon_transform = iradon(radon_transform, theta=thetha, circle=False, filter_name=None)
 iradon_transform = (iradon_transform - iradon_transform.min()) / (iradon_transform.max() - iradon_transform.min())
@@ -94,14 +103,17 @@ plt.savefig('iradon_transform.png', bbox_inches='tight', pad_inches=0)
 plt.close()
 
 # load the reconstructed image from reconstructed_image_lambda_0.1.txt
-with open('reconstructed_image.txt', 'r') as f:
-    reconstructed_image = np.array([[float(x) for x in line.split()] for line in f])
-print(rrmse(reconstructed_image, image1))
-exit()
-myART(coords, lr=lr, num_iterations=10)
-plt.plot(rrmses)
-plt.xlabel('Iteration')
-plt.ylabel('RRMSE')
-plt.title('RRMSE vs Iteration')
-plt.grid()
-plt.savefig(f'rrmse_plot_lambda_{lr}.png')
+# with open('reconstructed_image.txt', 'r') as f:
+#     reconstructed_image = np.array([[float(x) for x in line.split()] for line in f])
+# print(rrmse(reconstructed_image, image1))
+
+for lr in range(1, 11):
+    myART(coords, lr=lr/10, num_iterations=10)
+    plt.plot(rrmses)
+    plt.xlabel('Iteration')
+    plt.ylabel('RRMSE')
+    plt.title('RRMSE vs Iteration')
+    plt.grid()
+    plt.savefig(f'rrmse_plot_lambda_{lr}.png', bbox_inches='tight', pad_inches=0)
+    rrmses = []
+    exit()
